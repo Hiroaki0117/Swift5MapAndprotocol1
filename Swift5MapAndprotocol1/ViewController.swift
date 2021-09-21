793a82dd6186e08128c8d0b1016e6a1c67b30a44
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-09-22 08:52:39 +0900

    Initial Commit

---
+++ Swift5MapAndprotocol1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5MapAndprotocol1
+//
+//  Created by 西村拓晃 on 2021/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
